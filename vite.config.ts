336ef31cbe16171f6708041a5d274404d4d3aa84
amoussikepler <amoussi@github.com>
2025-12-11 00:44:04 +0100

    Change output directory to dist

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@
   export default defineConfig({
     plugins: [react()],
     build: {
-      outDir: 'build',
+      outDir: 'dist',
     },
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
